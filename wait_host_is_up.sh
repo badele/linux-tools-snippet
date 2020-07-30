f7ecc9bc3bfd050a5e494ea728dadaa90e692600
Bruno Adelé <bruno@adele.im>
2020-07-30 11:08:26 +0200

    Minor update

--- wait_host_is_up.sh
+++ wait_host_is_up.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # https://serverfault.com/a/42382/171697
+# # HOSTNAME=hostname./wait_host_is_up.sh.sh
+# One line => until ping -c1 1.2.3.4 >/dev/null 2>&1; do :; done & trap "kill $\!" SIGINT ; wait "$\!" ; trap - SIGINT
+
 
 wait_host_is_up () {
-    hostname=$1
-    until ping -c1 "$hostname" >/dev/null 2>&1; do :; done &    # The "&" backgrounds it
+    until ping -c1 "$HOSTNAME" >/dev/null 2>&1; do :; done &    # The "&" backgrounds it
     trap "kill $!" SIGINT
     wait $!          # Wait for the loop to exit, one way or another
     trap - SIGINT    # Remove the trap, now we're done with it
