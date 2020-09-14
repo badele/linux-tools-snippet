a6314b5997423e306a90bd3c71b542ab38bc8286
Bruno Adelé <bruno@adele.im>
2020-09-14 15:05:45 +0200

    Add conntrack tool

---
+++ conntrack_summary.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Show current conntrack summary
+
+while true; do
+    RESULT=$(conntrack -L)
+
+    date +"%Y-%m-%d %H:%M:%S"
+    echo "== Protocols =="
+    echo "$RESULT" | grep tcp | awk '{ print $1}' | sort | uniq -c | sort -n
+
+    echo "== TCP states =="
+    echo "$RESULT" | grep tcp | awk '{ print $4}' | sort | uniq -c | sort -n
+
+    echo "== TCP dst=="
+    echo "$RESULT" | grep tcp | awk '{ print $6}' | sort | uniq -c | sort -n
+
+    echo "== UDP dst=="
+    echo "$RESULT" | grep udp | awk '{ print $5}' | sort | uniq -c | sort -n
+
+    echo "===================================================================================="
+
+    sleep 1
+done
